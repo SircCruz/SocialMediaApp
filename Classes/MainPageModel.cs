2d7311b9c1f89dcce054a5dcd6d0dcaaa5698425
SircCruz <christophercruz936@gmail.com>
2022-06-20 16:02:31 +0800

    Deleted classes folder

--- Classes/MainPageModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SocialMediaApp.Classes
-{
-    public class MainPageModel
-    {
-        public IEnumerable<Models.PostsModel>? Model { get; set; }
-        public Models.PostsModel? Model2 { get; set; }
-    }
-}
